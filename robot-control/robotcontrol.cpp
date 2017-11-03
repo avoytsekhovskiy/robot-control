db2e85675cbb2cb62c7fda4e882058cf8b24d0d9
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2017-11-03 10:08:10 +0700

    add position parameters and method updatePos()

--- robot-control/robotcontrol.cpp
+++ robot-control/robotcontrol.cpp
@@ -23,29 +23,56 @@ const float f_width = 300;
 const float f_lenght = 300;
 // 
 
-float x = 0, y = 0;
 bool bTerminate = false;
 
 class Robot
 {
 private:
 	// радиус колеса 4 cm
-	int w_rad = 4;
+	int wheel_rad = 4;
 	// ширина колесной базы 15 cm
-	int r_width = 15;
+	int wheel_width = 15;
 	// положение левого энкодера
 	float l_enc = 0;
 	// положение правого энкодера
 	float r_enc = 0;
 	// запас прочности робота
 	float strength = 100;
+	// П-коэффициент регулятора
+	float kp = 5;
+	// И-коэффициент регулятора
+	float ki = 0.02;
+	// координаты робота: x - вверх, y - вправо
+	float x = 0, y = 0;
+	// абсолютный угол поворота робота
+	float abs_ang = 0;
+	// расстояние, пройденное за предыдущую итерацию
+	float past_dist = 0;
+	// угол, на который повернулся робот за предыдущую итерацию
+	float past_ang = 0;
 
+	// обновление данных о положении робота
+	void updatePos(float l_enc, float r_enc)
+	{
+		float l_dist, r_dist;
+		l_dist = (l_enc * 360) * 2 * M_PI * wheel_rad;
+		r_dist = (r_enc * 360) * 2 * M_PI * wheel_rad;
+
+		past_dist = (l_dist + r_dist) / 2;
+		past_ang = (l_dist - r_dist) / wheel_width;
+		abs_ang += past_ang;
+		x += past_dist * cos(abs_ang);
+		y += past_dist * sin(abs_ang);
+	}
+	
 public:
+	// прием сообщения с сервера
 	void receiveMess()
 	{
 		cout << "This method in progress";
 	}
 
+	// отправка сообщения на сервер
 	int sendMess(SOCKET s, int l_power, int r_power, int d_power, sockaddr_in serv_addr, int addrlen)
 	{
 		if (l_power > 100) l_power = 100;
@@ -56,6 +83,12 @@ public:
 		sprintf(control, "0xFF 0xFF L%d R%d D%d 0xEE", l_power, r_power, d_power);
 		cout << control << "\n";
 		return sendto(s, control, strlen(control), 0, (sockaddr*)&serv_addr, addrlen);
+	}
+
+	// регулирование движения робота
+	float regulator()
+	{
+		return 0;
 	}
 };
 
